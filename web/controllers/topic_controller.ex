67ebdda7ec131bd3356d5ef0add03d61485259ef
Rafael Camarda <rf.camarda@gmail.com>
2018-11-27 13:46:15 -0200

    create user and topic association

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,38 +3,51 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  plug Discuss.Plugs.RequireAuth when action in [
-    :new, :create, :edit, :update, :delete
-  ]
+  plug(
+    Discuss.Plugs.RequireAuth
+    when action in [
+           :new,
+           :create,
+           :edit,
+           :update,
+           :delete
+         ]
+  )
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
-    render conn, "index.html", topics: topics
+    render(conn, "index.html", topics: topics)
   end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
   end
 
   def edit(conn, %{"id" => topic_id}) do
     topic = Repo.get(Topic, topic_id)
     changeset = Topic.changeset(topic)
 
-    render conn, "edit.html", changeset: changeset, topic: topic
+    render(conn, "edit.html", changeset: changeset, topic: topic)
   end
 
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
+
+    changeset =
+      conn.assigns.user
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
+
       {:error, changeset} ->
-        render conn, "new.html", changeset: changeset
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
@@ -47,13 +60,14 @@ defmodule Discuss.TopicController do
         conn
         |> put_flash(:info, "Topic Updated")
         |> redirect(to: topic_path(conn, :index))
+
       {:error, changeset} ->
-        render conn, "edit.html", changeset: changeset, topic: old_topic
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
     end
   end
 
   def delete(conn, %{"id" => topic_id}) do
-    Repo.get!(Topic, topic_id) |> Repo.delete!
+    Repo.get!(Topic, topic_id) |> Repo.delete!()
 
     conn
     |> put_flash(:info, "Topic Deleted")
